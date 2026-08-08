ce4a67a5331310c9adceec514d9f5b5b4e642665
CodingYoungMan-Exp <codingwithenginesonline@gmail.com>
2026-08-08 20:33:22 +0530

    Update firebaseClient.js

--- src/lib/firebaseClient.js
+++ src/lib/firebaseClient.js
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIza...",
+apiKey: "AIzaSyAk-kVVZl-dKKPOyHtKzK4fcfBarAhogKA",
   authDomain: "focusflow-9cd0f.firebaseapp.com",
+  databaseURL: "https://focusflow-9cd0f-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "focusflow-9cd0f",
-  storageBucket: "focusflow-9cd0f.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  storageBucket: "focusflow-9cd0f.firebasestorage.app",
+  messagingSenderId: "605059562557",
+  appId: "1:605059562557:web:8fbf999894c3c648957e54",
+  measurementId: "G-ZQX9MEV4KJ"
 };
 
 const app = initializeApp(firebaseConfig);
